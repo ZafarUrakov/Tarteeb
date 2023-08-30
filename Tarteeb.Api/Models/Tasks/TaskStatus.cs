ec2c5c7537aeee017e889a4a4f60fa565d70a4af
Zafar Urakov <goliburakov535@gmail.com>
2023-08-31 01:39:17 +0900

    DATA: Add Tasks

---
+++ Tarteeb.Api/Models/Tasks/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+namespace Tarteeb.Api.Models.Tasks
+{
+    public enum TaskStatus
+    {
+        UNKNOWN,
+        TODO,
+        INPROGRESS,
+        DONE
+    }
+}
