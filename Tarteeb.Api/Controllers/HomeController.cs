064e4840796892104387c4c71f14758110e39b8b
Zafar Urakov <goliburakov535@gmail.com>
2023-08-30 21:16:24 +0900

    INFRA: Initialize API Project

---
+++ Tarteeb.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tarteeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetMessage() => "Tarteeb is Working";
+    }
+}
