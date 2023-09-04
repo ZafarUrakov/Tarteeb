7f474f768c3efd394a787742e5c76d4d9b6e91ea
Zafar <icom@MACKBOOK>
2023-09-04 20:48:44 +0300

    CODE RUB: Migrations Clean Excess Code

--- Tarteeb.Api/Migrations/20230903173132_AddUsers.cs
+++ Tarteeb.Api/Migrations/20230903173132_AddUsers.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
