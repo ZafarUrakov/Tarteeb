f8b8555a64f5641160b53ebcd2381658a9b6c3b2
Zafar <icom@MACKBOOK>
2023-09-04 21:31:39 +0300

    DATA: Model & Migrate Teams

---
+++ Tarteeb.Api/Brokers/Storages/StorageBroker.Teams.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+using Microsoft.EntityFrameworkCore;
+using Tarteeb.Api.Models.Teams;
+
+namespace Tarteeb.Api.Brokers.Storages
+{
+    public partial class StorageBrokerTeams
+    {
+        DbSet<Team> Teams { get; set; }
+    }
+}
---
+++ Tarteeb.Api/Migrations/20230904182306_AddTeams.cs
@@ -0,0 +1,46 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tarteeb.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Deadline",
+                table: "Tasks",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deadline",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Tasks");
+        }
+    }
+}
