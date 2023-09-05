58d17a7580cf080652449ffe0475036e4858b037
Zafar <icom@MACKBOOK>
2023-09-05 14:21:14 +0300

    INFRA: Initialize Test Project

---
+++ Tarteeb.UnitTests/DeleteMe.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Tarteeb.UnitTests
+{
+    public class DeleteMe
+    {
+        bool value = true;
+
+        [Fact]
+        public void ShuldBeTrue() => Assert.True(true);
+    }
+}
