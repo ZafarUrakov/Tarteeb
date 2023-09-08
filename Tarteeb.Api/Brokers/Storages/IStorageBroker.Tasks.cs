0573f61535d3d654016fd960086470ca093d5f42
Zafar <icom@MACKBOOK>
2023-09-08 20:20:15 +0300

    BROKERS: Delete Task

--- Tarteeb.Api/Brokers/Storages/IStorageBroker.Tasks.cs
+++ Tarteeb.Api/Brokers/Storages/IStorageBroker.Tasks.cs
@@ -3,9 +3,14 @@
 // Free to use to bring order in your workplace
 //===============================
 
+using System.Linq;
+using System.Threading.Tasks;
+using Local = Tarteeb.Api.Models.Tasks;
 namespace Tarteeb.Api.Brokers.Storages;
 
 public partial interface IStorageBroker_Tasks
 {
-    
+    ValueTask<Local.Task> InsertTaskAsync(Local.Task task);
+    IQueryable<Local.Task> SelectAllTasks();
+    ValueTask<Local.Task> DeleteTaskAsync(Local.Task task);
 }
