817c09922c4852aaf795346cd454bfb6f1479b4d
Zafar <icom@MACKBOOK>
2023-09-15 22:06:33 +0300

    CODE RUB: Run Code Cleanup

--- Tarteeb.Api/Program.cs
+++ Tarteeb.Api/Program.cs
@@ -10,10 +10,8 @@ namespace Tarteeb.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) =>
             CreateHostBuilder(args).Build().Run();
-        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
